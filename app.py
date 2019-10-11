995bdfa93f4282c40beea46b820ed835fb4d4dbc
somewheve <somewheve@gmail.com>
2019-10-11 08:14:52 +0800

    make it

--- app.py
+++ app.py
@@ -2,17 +2,22 @@ from ctpbee import CtpBee
 
 from action import ActionMe
 from risk import RiskMe
-from strategy import DataRecorder
+from ext import data_recorder
 
 
 def create_app():
     app = CtpBee("last", __name__, action_class=ActionMe, risk=RiskMe, refresh=True)
 
     app.config.from_pyfile("config.py")
-    data_recorder = DataRecorder("data_recorder", app)
+    data_recorder.init_app(app)
     return app
 
 
 if __name__ == '__main__':
+    # 24 小时监管运行
     from ctpbee import hickey
     hickey.start_all(app_func=create_app)
+
+    # 直接启动
+    app = create_app()
+    app.start()
--- ext.py
+++ ext.py
@@ -5,3 +5,7 @@
         strategy 在策略中导入实例进行使用
         app  在create_app函数中使用实例.init_app(app)
 """
+
+from strategy import DataRecorder
+
+data_recorder = DataRecorder("data_recorder")
